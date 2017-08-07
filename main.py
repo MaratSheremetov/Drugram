39e711dc5d0126b2c5886c030fc6ba97c851c0ab
MaratSheremetov <sheremetov_marat@mail.ru>
2017-08-07 09:33:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -328,11 +328,11 @@ def out_markets(call, user_id):
         data_market = cur.fetchall()
         ###############################
 
-<<<<<<< HEAD
+#<<<<<<< HEAD
         ## Вывод данных ##
 =======
     ## Output date ##
->>>>>>> 583ad9b460ec329bc189e085f4d929d540187a66
+#>>>>>>> 583ad9b460ec329bc189e085f4d929d540187a66
 
         markup_next = telebot.types.InlineKeyboardMarkup()
 
@@ -343,11 +343,11 @@ def out_markets(call, user_id):
         con.commit()
         data_pos = cur.fetchone()
 
-<<<<<<< HEAD
+#<<<<<<< HEAD
         print(data_pos[0])
 =======
     #print(data_pos[0])
->>>>>>> 583ad9b460ec329bc189e085f4d929d540187a66
+#>>>>>>> 583ad9b460ec329bc189e085f4d929d540187a66
 
         if (data_pos[0] == 0):
             if (len(data_market) - 1 == 0):
