ff830b7735c95d60db90d19abf82df85d2afb0da
MaratSheremetov <sheremetov_marat@mail.ru>
2017-08-19 10:27:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -339,16 +339,8 @@ def out_markets(call, user_id):
         cur.execute('SELECT pos FROM users_list WHERE user_id = ' + str(call.message.chat.id))
         con.commit()
         data_pos = cur.fetchone()
-<<<<<<< HEAD
         
 
-=======
-<<<<<<< HEAD
-
-=======
-        
->>>>>>> 5d16ce2c3f292efa9bd94508dbe005c375ef8512
->>>>>>> 64fe15bfe2071038f72496fe2ce19903ee47b603
 
         if (data_pos[0] == 0):
             if (len(data_market) - 1 == 0):
